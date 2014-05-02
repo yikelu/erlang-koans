57ed51a826b26a5877cf65d27862c45328e84d61
Yike Lu <yikelu.home@gmail.com>
2014-05-02 13:16:20 -0500

    Finish integers

--- src/about_integers.erl
+++ src/about_integers.erl
@@ -11,30 +11,30 @@
         ]).
 
 arithmetic_is_easily_applied() ->
-    10 - 5 =:= __.
+    10 - 5 =:= 5.
 
 remember_the_remainder() ->
-    11 rem 2 =:= __.
+    11 rem 2 =:= 1.
 
 division_is_not_quite_so_straightforward() ->
-    11 / 2 =:= __.
+    11 / 2 =:= 5.5.
 
 maybe_this_is_what_is_needed() ->
-    11 div 2 =:= __.
+    11 div 2 =:= 5.
 
 absolute_integers() ->
-    abs(-42) =:= __.
+    abs(-42) =:= 42.
 
 base_does_not_change_the_value() ->
-    12#36 =:= __.
+    12#36 =:= 42.
 
 this_makes_applying_bitwise_operations_a_breeze() ->
     2#01011 band
-    2#11010 =:= __.
+    2#11010 =:= 2#01010.
 
 do_the_binary_shuffle() ->
-    2#1010 bsl 2#101 =:= __.
+    2#1010 bsl 2#101 =:= 2#101000000.
 
 now_to_the_right() ->
-    2#1010 bsr 2#1 =:= __.
+    2#1010 bsr 2#1 =:= 2#0101.
 
